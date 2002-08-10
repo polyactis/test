60b269ae5baa37b3090bad472718eed33e06a3a0
gtk-user <gtk-user@1f031e46-900f-0410-8561-9f4ed0c18923>
2002-08-10 20:54:08 +0000

    Remove the trash of thread attrbute segment.
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@11 1f031e46-900f-0410-8561-9f4ed0c18923

--- trunk/thread-create.c
+++ trunk/thread-create.c
@@ -15,14 +15,9 @@ int main()
 {
   int i;
   pthread_t thread_id;
-  //pthread_attr_t attr;
-
-  //pthread_attr_init(&attr);
-  //pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);
   
   pthread_create (&thread_id,NULL,&print_x,NULL);
 
-  //pthread_attr_destroy(&attr);
   pthread_join(thread_id,NULL);
   
   for(i=0;i<100;i++)
