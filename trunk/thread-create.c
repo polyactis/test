fc5953e30f426b93765fc04ad962ecab2335ad0c
gtk-user <gtk-user@1f031e46-900f-0410-8561-9f4ed0c18923>
2002-08-10 09:50:15 +0000

    Thread "detached" attribute added for research.
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@7 1f031e46-900f-0410-8561-9f4ed0c18923

--- trunk/thread-create.c
+++ trunk/thread-create.c
@@ -15,10 +15,17 @@ int main()
 {
   int i;
   pthread_t thread_id;
+  //pthread_attr_t attr;
 
+  //pthread_attr_init(&attr);
+  //pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);
+  
   pthread_create (&thread_id,NULL,&print_x,NULL);
 
-  for(i=0;i<1000000;i++)
+  //pthread_attr_destroy(&attr);
+  pthread_join(thread_id,NULL);
+  
+  for(i=0;i<100;i++)
     fputc('0',stderr);
 
   return 0;
--- trunk/thread_create2.c
+++ trunk/thread_create2.c
@@ -22,6 +22,10 @@ int main()
 {
   pthread_t thread1_id;
   pthread_t thread2_id;
+
+  pthread_attr_t attr1;
+  pthread_attr_t attr2;
+
   struct char_print_parameters thread1_param;
   struct char_print_parameters thread2_param;
 
@@ -30,11 +34,15 @@ int main()
 
   thread2_param.character='o';
   thread2_param.count=10000;
-  pthread_create(&thread1_id,NULL,&char_print,&thread1_param);
+
+  pthread_attr_init(&attr1);
+  pthread_attr_setdetachstate(&attr1,PTHREAD_CREATE_DETACHED);
+
+  pthread_create(&thread1_id,&attr1,&char_print,&thread1_param);
   pthread_create(&thread2_id,NULL,&char_print,&thread2_param);
 
-  pthread_join(thread1_id,NULL);
   pthread_join(thread2_id,NULL);
+  pthread_attr_destroy(&attr1);
 
   return 0;
 }
