bfb1b8b75ba2716f48a4b041d922a751e5d69463
yh <yh@1f031e46-900f-0410-8561-9f4ed0c18923>
2005-02-08 07:08:22 +0000

    add testing for vector, hash_map, unary_function
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@33 1f031e46-900f-0410-8561-9f4ed0c18923

--- trunk/gsl/gsl_example.cc
+++ trunk/gsl/gsl_example.cc
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <ostream>
+#include <vector>
+#include <hash_map.h>
 #include <gsl/gsl_sf_bessel.h>
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_cdf.h>
 using namespace std;
+
+template<class T> struct print: public unary_function<T, void>
+{
+	print(ostream& out): os(out), count(0){}
+	void operator()(T x){x = 1.0;os<<x<<' ';++count;}
+	ostream& os;
+	int count;
+};
 
 int main(void)
 {
@@ -42,9 +53,30 @@ int main(void)
 	double q_c = gsl_cdf_chisq_Q(2, 10);
 	cout <<q_c<<endl;
 
-	//sqrt 
-	double sq = sqrt(2.0);
-	cout <<"Square root of 2.0 is "<<sq<<endl;
+	//vector, for_each and unary_function
+	vector<float> vf;
+	vf.resize(10);
+	vf.push_back(GSL_NAN);
+	cout<<"size of the vector:"<<vf.size()<<endl;
+	for_each(vf.begin(),vf.end(),print<float>(cout));
+	cout<<endl;
+	vector<vector<float> > gene_array;
+	gene_array.resize(100);
+	if(gene_array[0].size()==0)
+		cout<<"no values for gene:"<<0<<endl;
+	
+	//hash_map
+	hash_map<const char*, float > gene_label2index;
+	
+	gene_label2index["YAR006W"] = 0.001;
+	//hash_map<const char*, float>::iterator result = gene_label2index.find("YAR006W");
+	float value = gene_label2index["YAR006X"];
+	if(gene_label2index["YAR006W"])
+		cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;
+	if(value)
+		cout <<"expression value of YAR006W is "<< gene_label2index["YAR006X"]<<endl;
 	return 0;
 	
 }
