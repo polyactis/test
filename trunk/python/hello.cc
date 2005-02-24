0569975c0fb5a7b6c825e89af5f2cd278f33b6d6
yh <yh@1f031e46-900f-0410-8561-9f4ed0c18923>
2005-02-24 11:17:29 +0000

    02-24-05. see doc in the file have this timestamp
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@43 1f031e46-900f-0410-8561-9f4ed0c18923

--- trunk/python/hello.cc
+++ trunk/python/hello.cc
@@ -1,17 +1,71 @@
 /*
- *Boost.Python's hello world example
- */
-
+*Boost.Python's hello world example
+*
+*02-24-05 test dict, list, tuple python data structures.
+*
+*
+*/
+#include <iostream>
 #include <boost/python.hpp>
-using namespace boost::python;
+#include <boost/graph/subgraph.hpp>	//for boost::subgraph
 
+using namespace boost::python;
+//using namespace boost;		//02-24-05 namespace conflict
 char const* greet()
 {
-   return "hello, world";
+	return "hello, world";
+}
+
+class World
+{
+	public:
+	World(std::string msg): msg(msg) {} // added constructor
+	void set(std::string msg) { this->msg = msg; }
+	std::string greet() { return msg; }
+	boost::python::tuple return_keys(dict dic, int length);
+	std::string msg;
+};
+
+boost::python::tuple World::return_keys(dict dic, int length)
+{
+	dict dc1;
+	dict dc2;
+	list k_list = dic.keys();
+	for(int i=0; i<length; i++)
+		std::cout<<extract<int>(k_list[i][0])<<"\t";
+	std::cout<<std::endl;
+	int len = extract<int>(k_list.attr("__len__")());
+	std::cout<<"length of the dictionary is "<<len<<std::endl;
+	for(int i=0; i<len;i++)
+	{
+		int g1 = extract<int>(k_list[i][0]);
+		int g2 = extract<int>(k_list[i][1]);
+		boost::python::tuple tup = boost::python::make_tuple(g1,g2);
+		std::string s =extract<std::string>(dic[tup]);
+		std::cout<<s<<std::endl;
+	}
+	dc1[make_tuple(1,2)]=1;
+	dc2[make_tuple(2,3)]=2;
+	list new_list;
+	new_list.append(dc1);
+	new_list.append(dc2);
+	return make_tuple(new_list);
+	
 }
 
 
+void f(str name)
+{
+    object n2 = name.attr("upper")();   // NAME = name.upper()
+    str NAME = name.upper();            // better
+    object msg = "%s is bigger than %s" % make_tuple(NAME,name);
+}
+
 BOOST_PYTHON_MODULE(hello)
 {
-    def("greet", greet);
+	class_<World>("World", init<std::string>())
+		.def("greet", &World::greet)
+		.def("set", &World::set)
+		.def("return_keys", &World::return_keys)
+	;
 }
