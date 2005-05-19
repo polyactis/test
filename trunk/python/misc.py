7d8f8d9ce34a9ac8ac3773b5e5f7941ae018c9ed
yh <yh@1f031e46-900f-0410-8561-9f4ed0c18923>
2005-05-19 08:22:25 +0000

    add lots of ad hoc functions
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@55 1f031e46-900f-0410-8561-9f4ed0c18923

--- trunk/python/misc.py
+++ trunk/python/misc.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 04-27-05
 	miscellaneous functions used in python's interactive mode
@@ -16,6 +17,7 @@ def take_list(source_list,index_list):
 	new_list = []
 	for i in index_list:
 		new_list.append(source_list[i])
+	return new_list
 
 def return_seed_array(curs, edge_id_list, col_index_list):
 	"""
@@ -102,6 +104,7 @@ def db_connect(psycopg, hostname, dbname, schema=None):
 	03-08-05
 		parameter schema is optional
 	"""
+	import psycopg
 	conn = psycopg.connect('host=%s dbname=%s'%(hostname, dbname))
 	curs = conn.cursor()
 	if schema:
@@ -255,8 +258,403 @@ class get_edge_data:
 		del writer
 		file.close()
 
+def return_gene_set_list(dir):
+	import sys, os, csv
+	from sets import Set
+	files = os.listdir(dir)
+	files.sort()
+	sys.stderr.write("\tTotally, %d files to be processed.\n"%len(files))
+	gene_set_list = []
+	for f in files:
+		local_gene_set = Set()
+		print f
+		#sys.stderr.write("%d/%d:\t%s\n"%(files.index(f)+1,len(files),f))
+		f_path = os.path.join(dir, f)
+		reader = csv.reader(file(f_path), delimiter='\t')
+		for row in reader:
+			local_gene_set.add(row[0])
+		gene_set_list.append(local_gene_set)
+		del reader
+	return gene_set_list
+
+def cal_set_intersection(gene_set_list, outfname):
+	import sys, csv
+	writer = csv.writer(open(outfname,'w'), delimiter='\t')
+	counter = len(gene_set_list)
+	for i in range(counter):
+		ls = ['']*counter
+		ls[i] = len(gene_set_list[i])
+		for j in range(i+1, counter):
+			ls[j] = len(gene_set_list[i]&gene_set_list[j])
+		writer.writerow(ls)
+	
+def known_gene_percentage(gene_set, known_gene_dict):
+	counter = 0
+	for gene in gene_set:
+		if gene in known_gene_dict:
+			counter+=1
+	
+	print counter
+
+"""
+05-12-05
+	the filename is fetched from NCBI GEO, which has an interface
+	to let you get the GDS's given a platform_id
+	
+	The function parses the text file and get the GDS ids.
+"""
+def return_gds_list(filename):
+	import re, os
+	p_gds = re.compile(r'(GDS\d+)\ ')
+	gds_list = []
+	inf = open(filename, 'r')
+	for line in inf:
+		p_gds_result = p_gds.search(line)
+		if p_gds_result:
+			gds_list.append(p_gds_result.groups()[0])
+	inf.close()
+	return gds_list
+
+"""
+05-12-05
+	get the dataset_ids from the database given a GPL id.
+"""
+def return_old_gpl_list(curs, gpl):
+	curs.execute("select dataset_id from ds_stat where platform_id ='%s' and total_samples>=8"%(gpl))
+	rows = curs.fetchall()
+	old_gpl_list = []
+	for row in rows:
+		old_gpl_list.append(row[0])
+	return old_gpl_list
+
+"""
+05-12-05
+	output the gds_list in ftp url form to let wget automatically download all files
+"""
+def output_gds_url(gds_list, filename):
+	outf = open(filename, 'w')
+	url_prefix = 'ftp://anonymous:243242fa@ftp.ncbi.nih.gov/pub/geo/data/gds/soft_gz/'
+	for gds in gds_list:
+		line = '%s%s.soft.gz'%(url_prefix, gds)
+		outf.write('%s\n'%line)
+	outf.close()
+
+"""
+05-12-05
+	output GDS's from mdb given  gds_id_list
+"""
+def output_gds_id_list(gds_id_list, output_dir, prefix):
+	from microarraydb import microarraydb
+	import os
+	instance = microarraydb('mdb', 1)	#1 means normalization required
+	for gds_id in gds_id_list:
+		ofname = '%s_%s'%(prefix, gds_id)
+		ofname = os.path.join(output_dir, ofname)
+		instance.geo_single(gds_id, ofname)
+
+"""
+05-16-05
+	
+"""
+def return_gene_no2is_correct_lca(input_fname, hostname='zhoudb', dbname='graphdb', schema='sc_new_38'):
+	gene_no2is_correct_lca = {}
+	p_gene_table = "p_gene_%s_e5"%input_fname
+	gene_p_table = "gene_p_%s_e5_p01"%input_fname
+	from codense.common import db_connect
+	import psycopg
+	conn, curs = db_connect(hostname, dbname, schema)
+	curs.execute("select p.p_gene_id, p.gene_no, p.is_correct_lca from %s p, %s g where g.p_gene_id=p.p_gene_id"%\
+		(p_gene_table, gene_p_table))
+	rows = curs.fetchall()
+	for row in rows:
+		gene_no = row[1]
+		is_correct_lca = row[2]
+		if is_correct_lca==-1:
+			continue
+		if gene_no not in gene_no2is_correct_lca:
+			gene_no2is_correct_lca[gene_no] = []
+		gene_no2is_correct_lca[gene_no].append(is_correct_lca)
+	return gene_no2is_correct_lca
+
+def extra_gene_accuracy(gene_no2is_correct_lca_1, gene_no2is_correct_lca_2):
+	from sets import Set
+	gene_no_set1 = Set(gene_no2is_correct_lca_1.keys())
+	gene_no_set2 = Set(gene_no2is_correct_lca_2.keys())
+	
+	extra_gene_set = gene_no_set2 - gene_no_set1
+	is_correct_lca_list = []
+	for gene in extra_gene_set:
+		is_correct_lca_list += gene_no2is_correct_lca_2[gene]
+	print "No of extra genes: %s"%len(extra_gene_set)
+	print "Total predictions: %s"%len(is_correct_lca_list)
+	print "Accuracy: %s"%(float(sum(is_correct_lca_list))/len(is_correct_lca_list))
+
+"""
+05-12-05
+	additional GDS's
+
+>>> Set(new_gpl81_list)-Set(old_gpl81_list)
+Set(['GDS879', 'GDS857', 'GDS951', 'GDS717', 'GDS734', 'GDS912', 'GDS658', 'GDS739', 'GDS812', 'GDS679', 'GDS771', 'GDS773', 'GDS794', 'GDS684', 'GDS615', 'GDS614', 'GDS639', 'GDS638', 'GDS907', 'GDS782', 'GDS967', 'GDS788', 'GDS828', 'GDS640', 'GDS641', 'GDS681', 'GDS683', 'GDS648', 'GDS890', 'GDS604', 'GDS882', 'GDS605', 'GDS663', 'GDS660'])
+>>> Set(new_gpl85_list)-Set(old_gpl85_list)
+Set(['GDS965', 'GDS869', 'GDS769', 'GDS621', 'GDS778', 'GDS955', 'GDS956', 'GDS877', 'GDS599', 'GDS589', 'GDS872', 'GDS657', 'GDS656', 'GDS808', 'GDS654', 'GDS174', 'GDS598', 'GDS710', 'GDS765'])
+>>> Set(new_gpl96_list)-Set(old_gpl96_list)
+Set(['GDS834', 'GDS855', 'GDS690', 'GDS858', 'GDS737', 'GDS730', 'GDS914', 'GDS810', 'GDS916', 'GDS917', 'GDS738', 'GDS715', 'GDS686', 'GDS906', 'GDS962', 'GDS785', 'GDS901', 'GDS946', 'GDS826', 'GDS749', 'GDS748', 'GDS556', 'GDS760'])
+
+"""
+
+"""
+05-16-05
+	transform the cor_vector file (haiyan's copath input format) into the format similar to microarray datasets
+	to do graph construction.
+	use sig_fname to judge whether one edge meets the min_support
+"""
+def haiyan_cor_vector_file2graph_modeling_input(cor_fname, sig_fname, output_fname, min_support=6):
+	import csv
+	cor_reader = csv.reader(file(cor_fname), delimiter='\t')
+	sig_reader = csv.reader(file(sig_fname), delimiter='\t')
+	writer = csv.writer(open(output_fname, 'w'), delimiter='\t')
+	cor_row = cor_reader.next()
+	sig_row = sig_reader.next()
+	while 1:
+		edge_id = '%s_%s'%(cor_row[0], cor_row[1])	#concatenate the first two gene indices to form an edge
+		ls = [edge_id]	#later write it to the output_fname
+		sig_ls = sig_row[2:]
+		sig_ls = map(int, sig_ls)
+		support = sum(sig_ls)
+		if support>min_support:
+			for cor in cor_row[2:]:
+				if cor=='1100':
+					ls.append('NA')
+				else:
+					ls.append('%s'%(int(cor)*0.001))	#use string formatting to avoid too long points
+			writer.writerow(ls)
+		try:
+			cor_row = cor_reader.next()
+			sig_row = sig_reader.next()
+		except StopIteration:
+			break
+	del writer
+
+"""
+05-17-05
+	return the set of vertices covered by a mcl table
+"""
+def return_mcl_table_vertex_set(mcl_table, schema):
+	hostname = 'zhoudb'
+	dbname = 'graphdb'
+	from codense.common import db_connect
+	import psycopg, sys
+	conn, curs = db_connect(hostname, dbname, schema)
+	from sets import Set
+	gene_no_set = Set()
+	curs.execute("select vertex_set from %s"%mcl_table)
+	curs.execute("DECLARE crs CURSOR FOR select vertex_set from %s"%(mcl_table))
+	curs.execute("fetch 5000 from crs")
+	rows = curs.fetchall()
+	counter = 0
+	while rows:
+		for row in rows:
+			vertex_set = row[0]
+			vertex_list = vertex_set[1:-1].split(',')
+			vertex_list = map(int, vertex_list)
+			gene_no_set |= Set(vertex_list)
+			counter+=1
+		sys.stderr.write("%s%s"%("\x08"*20,counter))
+		curs.execute("fetch 5000 from crs")
+		rows = curs.fetchall()
+	return gene_no_set
+
+"""
+05-18-05
+"""
+def gene_no_set_from_p_gene_table(input_fname, hostname='zhoudb', dbname='graphdb', schema='sc_new_38'):
+	from sets import Set
+	gene_no_set = Set()
+	p_gene_table = "p_gene_%s_e5"%input_fname
+	gene_p_table = "gene_p_%s_e5_p01"%input_fname
+	from codense.common import db_connect
+	import psycopg
+	conn, curs = db_connect(hostname, dbname, schema)
+	curs.execute("select gene_no from %s"%\
+		(p_gene_table))
+	rows = curs.fetchall()
+	for row in rows:
+		gene_no = row[0]
+		gene_no_set.add(gene_no)
+	return gene_no_set
+
+"""
+05-18-05
+	not depend on curs,
+"""
+def get_gene_no2gene_id(hostname='zhoudb', dbname='graphdb', schema=None, table='gene'):
+	from codense.common import db_connect
+	import psycopg, sys
+	conn, curs = db_connect(hostname, dbname, schema)
+	sys.stderr.write("Getting gene_no2gene_id...")
+	gene_no2gene_id = {}
+	if schema:
+		curs.execute("set search_path to %s"%schema)
+	curs.execute("select gene_no, gene_id from %s"%table)
+	rows = curs.fetchall()
+	for row in rows:
+		gene_no2gene_id[row[0]] = row[1]
+	sys.stderr.write("Done\n")
+	return gene_no2gene_id
+
+"""
+05-18-05
+	
+"""
+def return_mcl_cluster_size_list(input_fname, hostname='zhoudb', dbname='graphdb', schema=None):
+	mcl_table = 'mcl_%s'%input_fname
+	from codense.common import db_connect
+	import psycopg, sys
+	conn, curs = db_connect(hostname, dbname, schema)
+	curs.execute("select array_upper(vertex_set,1) from %s"%mcl_table)
+	rows = curs.fetchall()
+	cluster_size_list = []
+	for row in rows:
+		cluster_size_list.append(row[0])
+	return cluster_size_list
+
+"""
+05-18-05
+"""
+def return_mcl_cluster_density_list(input_fname, hostname='zhoudb', dbname='graphdb', schema=None):
+	splat_table = 'splat_%s'%input_fname
+	mcl_table = 'mcl_%s'%input_fname
+	from codense.common import db_connect
+	import psycopg, sys
+	conn, curs = db_connect(hostname, dbname, schema)
+	curs.execute("select s.connectivity from %s m, %s s where s.splat_id =m.splat_id"%(mcl_table, splat_table))
+	rows = curs.fetchall()
+	ls = []
+	for row in rows:
+		ls.append(row[0])
+	return ls
+
+"""
+05-18-05
+"""
+def return_mcl_density_original_list(input_fname, hostname='zhoudb', dbname='graphdb', schema=None):
+	splat_table = 'splat_%s'%input_fname
+	mcl_table = 'mcl_%s'%input_fname
+	from codense.common import db_connect
+	import psycopg, sys
+	conn, curs = db_connect(hostname, dbname, schema)
+	curs.execute("select connectivity_original from %s"%(mcl_table))
+	rows = curs.fetchall()
+	ls = []
+	for row in rows:
+		ls.append(row[0])
+	return ls
+
+"""
+05-18-05
+"""
+def cluster_id_set_from_p_gene_table(input_fname, hostname='zhoudb', dbname='graphdb', schema='sc_new_38'):
+	from sets import Set
+	set_to_return = Set()
+	p_gene_table = "p_gene_%s_e5"%input_fname
+	gene_p_table = "gene_p_%s_e5_p01"%input_fname
+	from codense.common import db_connect
+	import psycopg
+	conn, curs = db_connect(hostname, dbname, schema)
+	curs.execute("select mcl_id from %s"%\
+		(p_gene_table))
+	rows = curs.fetchall()
+	for row in rows:
+		gene_no = row[0]
+		set_to_return.add(gene_no)
+	return set_to_return
+
+
+def cluster_size_list_from_p_gene_table(input_fname, hostname='zhoudb', dbname='graphdb', schema='sc_new_38'):
+	from sets import Set
+	set_to_return = Set()
+	p_gene_table = "p_gene_%s_e5"%input_fname
+	gene_p_table = "gene_p_%s_e5_p01"%input_fname
+	mcl_table = 'mcl_%s'%input_fname
+	from codense.common import db_connect
+	import psycopg
+	conn, curs = db_connect(hostname, dbname, schema)
+	curs.execute("select m.mcl_id,array_upper(m.vertex_set,1) from %s m, %s p where p.mcl_id=m.mcl_id"%\
+		(mcl_table, p_gene_table))
+	rows = curs.fetchall()
+	ls_to_return = []
+	for row in rows:
+		mcl_id = row[0]
+		if mcl_id not in set_to_return:
+			set_to_return.add(mcl_id)
+			ls_to_return.append(row[1])
+	return ls_to_return
+
+
+def cluster_id_size_list_from_p_gene_table(input_fname, hostname='zhoudb', dbname='graphdb', schema='sc_new_38'):
+	from sets import Set
+	set_to_return = Set()
+	p_gene_table = "p_gene_%s_e5"%input_fname
+	gene_p_table = "gene_p_%s_e5_p01"%input_fname
+	mcl_table = 'mcl_%s'%input_fname
+	from codense.common import db_connect
+	import psycopg
+	conn, curs = db_connect(hostname, dbname, schema)
+	curs.execute("select m.mcl_id,array_upper(m.vertex_set,1) from %s m, %s p where p.mcl_id=m.mcl_id"%\
+		(mcl_table, p_gene_table))
+	rows = curs.fetchall()
+	ls_to_return = []
+	for row in rows:
+		mcl_id = row[0]
+		if mcl_id not in set_to_return:
+			set_to_return.add(mcl_id)
+			ls_to_return.append(row)
+	return ls_to_return
+"""
+05-18-05
+"""
+
+def small_summary_graph(summary_gfile, cluster_edge_file, output_fname):
+	import csv,sys
+	summary_gf = open(summary_gfile,'r')
+	cluster_edge_f = open(cluster_edge_file, 'r')
+	of = open(output_fname,'w')
+	edge_list = cluster_edge_f.readline()
+	edge_list = edge_list[2:-3].split('},{')
+	for i in range(len(edge_list)):
+		edge_list[i]  = edge_list[i].split(',')
+		edge_list[i] = map(int, edge_list[i])
+	for line in summary_gf:
+		if line[0]!='e':
+			of.write(line)
+		else:
+			break
+	for edge in edge_list:
+		of.write('e %s %s 10\n'%(edge[0], edge[1]))
+	of.close()
+	
+"""
+05-18-05
+	protein efetch xml handler
+"""
+from xml.sax import handler
+from xml import sax
+class efetch_handler(handler.ContentHandler):
+	def __init__(self):
+		handler.ContentHandler.__init__(self)
+		self.GBSeq_comment = 0
+		self.comment = ''
+	def characters(self, s):
+		if self.GBSeq_comment == 1:
+			self.comment = s
+	def startElement(self, name, attrs):
+		if name == "GBSeq_comment":
+			self.GBSeq_comment = 1
+	def endElement(self, name):
+		if name == "GBSeq_comment":
+			self.GBSeq_comment = 0
 
 
 if __name__ == '__main__':
-	instance = get_edge_data()
-	instance.run()
+	import sys
+	haiyan_cor_vector_file2graph_modeling_input(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4]))
