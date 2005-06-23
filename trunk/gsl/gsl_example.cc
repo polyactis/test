31f2a6d89508ca5ee313b79159c0ec98d63a0719
yh <yh@1f031e46-900f-0410-8561-9f4ed0c18923>
2005-06-23 06:35:45 +0000

    test 1.<queue> priority_queue 2.<algorithm> sort, for_each    06-22-05 3.#include "boost/tuple/tuple.hpp"      //for boost::tuple      06-22-05 4.binary predicate, and unary functor
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@61 1f031e46-900f-0410-8561-9f4ed0c18923

--- trunk/gsl/gsl_example.cc
+++ trunk/gsl/gsl_example.cc
@@ -6,6 +6,9 @@
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_cdf.h>
+#include <queue>	//for priority_queue	06-22-05
+#include <algorithm>	//for sort, for_each	06-22-05
+#include "boost/tuple/tuple.hpp"	//for boost::tuple	06-22-05
 using namespace std;
 using namespace __gnu_cxx;
 
@@ -16,10 +19,34 @@ using namespace __gnu_cxx;
  * 
  */
 
+typedef boost::tuple<std::string, std::string, float> edge_string_cor;
+
+class cmp_edge	//06-22-05
+{
+public:
+    bool operator() (const edge_string_cor & s1, const edge_string_cor & s2) const
+    {
+        return s1.get<2>() > s2.get<2>();	//compare the 2th element float value correlation
+    }
+};
+
+//06-22-05	a binary predicate functor
+struct small_edge : public binary_function<edge_string_cor, edge_string_cor, bool> {
+	bool operator()(edge_string_cor x, edge_string_cor y) { return x.get<2>() < y.get<2>(); }
+};
+
 template<class T> struct print: public unary_function<T, void>
 {
 	print(ostream& out): os(out), count(0){}
 	void operator()(T x){x = 1.0;os<<x<<' ';++count;}
+	ostream& os;
+	int count;
+};
+
+struct print_edge_string_cor: public unary_function<edge_string_cor, void>	//06-22-05	output an edge.
+{
+	print_edge_string_cor(ostream& out): os(out), count(0){}
+	void operator()(edge_string_cor x){os<<x.get<0>()<<':'<<x.get<1>()<<"="<<x.get<2>()<<'\n';++count;}
 	ostream& os;
 	int count;
 };
@@ -87,6 +114,34 @@ int main(void)
 		cout<<"NO"<<endl;
 	if(value)
 		cout <<"expression value of YAR006W is "<< gene_label2index["YAR006X"]<<endl;
-	return 0;
 	
+	//06-22-05	test priority_queue
+	
+	priority_queue<edge_string_cor, vector<edge_string_cor>, small_edge > PQ;	//small_edge or cmp_edge are all ok. but not cmp_edge(), different from sort or other algorithm.
+	PQ.push(boost::make_tuple("Mm.1","Mm.2",0.87));
+	PQ.push(boost::make_tuple("Mm.2", "Mm.3", 0.57));
+	PQ.push(boost::make_tuple("Mm.3", "Mm.5", 0.78));
+	PQ.push(boost::make_tuple("Mm.5", "Mm.6", 0.57));
+	edge_string_cor top_element = PQ.top();
+	cout << "The top element of Q is "<<top_element.get<0>()<<":"<<top_element.get<1>()<<"="<<top_element.get<2>() << std::endl;
+	PQ.pop();
+	top_element = PQ.top();
+	cout <<"After a pop, the top element of Q is "<<top_element.get<0>()<<":"<<top_element.get<1>()<<"="<<top_element.get<2>() << std::endl;
+	PQ.pop();
+	top_element = PQ.top();
+	cout <<"pop again, then the top element of Q is "<<top_element.get<0>()<<":"<<top_element.get<1>()<<"="<<top_element.get<2>() << std::endl;
+	
+	
+	//06-22-05	test the binary predicate, cmp_edge and the unary_function, print_edge_string_cor.
+	vector<edge_string_cor> Q;
+	Q.push_back(boost::make_tuple("Mm.1","Mm.2",0.87));
+	Q.push_back(boost::make_tuple("Mm.2", "Mm.3", 0.57));
+	Q.push_back(boost::make_tuple("Mm.3", "Mm.5", 0.78));
+	Q.push_back(boost::make_tuple("Mm.5", "Mm.6", 0.57));
+	sort (Q.begin(), Q.end(), cmp_edge());
+	std::cout<<"edge_string_cor Vector  after sorted:"<<std::endl;
+	std::for_each(Q.begin(), Q.end(), print_edge_string_cor(std::cout));
+	
+	return 0;
+  
 }
