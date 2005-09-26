357c7ecfff0cfe8dc509b00c7ed9dcde584f1834
yh <yh@1f031e46-900f-0410-8561-9f4ed0c18923>
2005-09-26 08:36:31 +0000

    add some functions
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@67 1f031e46-900f-0410-8561-9f4ed0c18923

--- trunk/R/misc.R
+++ trunk/R/misc.R
@@ -8,3 +8,26 @@ draw_multiple_panel <- function(filename)
 	xyplot(acc1~recurrence|p_value,data=data)
 	data
 }
+
+#06-29-05
+trellis_set_background <- function(color)
+{
+	library(lattice)
+	background=trellis.par.get("background")
+	background$col=color
+	trellis.par.set("background",background)
+}
+
+draw_multiple_panel <- function(filename)
+{
+	library(lattice)
+	data = read.table(filename,header=TRUE)
+	xyplot(acc1~parameter,data=data, col="black")
+}
+
+draw_multiple_panel2 <- function(filename)
+{
+	library(lattice)
+	data = read.table(filename,header=TRUE)
+	xyplot(acc1~parameter2|parameter1,data=data, col="black")
+}
