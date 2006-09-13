d645acca4e1178889b53ee62a109a470cf2bf7ff
yh <yh@1f031e46-900f-0410-8561-9f4ed0c18923>
2006-09-13 06:20:59 +0000

    add draw_GO_informative_node_structure_graph()
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@150 1f031e46-900f-0410-8561-9f4ed0c18923

--- trunk/python/misc.py
+++ trunk/python/misc.py
@@ -4565,6 +4565,51 @@ def go_correlation_coherence(cor_fname, go_fname, output_dir):
 		fig_fname = '%s_go_id_%s.png'%(cor_fname, go_id)
 		print fig_fname
 		pylab.savefig(os.path.join(output_dir,fig_fname))
+
+
+"""
+2006-09-12
+	
+	call get_go_id2gene_set() from upstairs
+"""
+def draw_GO_informative_node_structure_graph(go_fname, fig_output_fname, sharing_threshold=30):
+	go_id2gene_set = get_go_id2gene_set(go_fname)
+	import networkx as nx
+	import pylab
+	g = nx.XGraph()
+	go_id_list = go_id2gene_set.keys()
+	no_of_go_ids = len(go_id_list)
+	for i in range(no_of_go_ids):
+		for j in range(i+1, no_of_go_ids):
+			go_id1 = go_id_list[i]
+			go_id2 = go_id_list[j]
+			no_of_sharing_genes = len(go_id2gene_set[go_id1]&go_id2gene_set[go_id2])
+			if no_of_sharing_genes>=sharing_threshold:
+				g.add_edge(go_id1, go_id2, no_of_sharing_genes)
+	edge_width_list = []
+	for (u, v, d) in g.edges():
+		edge_width_list.append(d/5)
+	node_size_list = []
+	for v in g:
+		node_size_list.append(len(go_id2gene_set[v])*4)
+	pos=nx.graphviz_layout(g)
+	pylab.figure(1, figsize=(25,25))
+	nx.draw_networkx_edges(g,pos,
+		alpha=0.3,
+		width=edge_width_list,
+		edge_color='m')
+	nx.draw_networkx_nodes(g,pos,
+		node_size=node_size_list,
+		node_color='r',
+		alpha=0.4)
+	nx.draw_networkx_edges(g,pos,
+		alpha=0.4,
+		node_size=0,
+		width=1,
+		edge_color='k')
+	nx.draw_networkx_labels(g, pos, fontsize=6)
+	pylab.title(go_fname)
+	pylab.savefig(fig_output_fname, dpi=75)
 	
 
 """
