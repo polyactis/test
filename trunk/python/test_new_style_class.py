ebe976868c3fc24fdd3896db9fd5920c303b440a
yh <yh@1f031e46-900f-0410-8561-9f4ed0c18923>
2008-05-10 07:17:51 +0000

    test some property thingy of python
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@212 1f031e46-900f-0410-8561-9f4ed0c18923

--- trunk/python/test_new_style_class.py
+++ trunk/python/test_new_style_class.py
@@ -19,6 +19,7 @@ class classa(object):
 		classa.func0(a,b)
 		cls.func0(a,b)
 		cls.func2(classa(0), a, b)	#TypeError: unbound method func2() must be called with classa instance as first argument (got str instance instead)
+		
 	func3 = classmethod(func3)
 	
 	def func1(a,b):
@@ -32,3 +33,51 @@ class classa(object):
 	func1 = staticmethod(func1)
 
 classa.func1('a',2)
+
+"""
+05/09/08
+	block below from http://users.rcn.com/python/download/Descriptor.htm
+"""
+class RevealAccess(object):
+	"""
+	05/09/08
+		from http://users.rcn.com/python/download/Descriptor.htm
+	A data descriptor that sets and returns values
+	   normally and prints a message logging their access.
+	"""
+	
+	def __init__(self, initval=None, name='var'):
+		self.val = initval
+		self.name = name
+	
+	def __get__(self, obj, objtype):
+		print 'Retrieving', self.name
+		return self.val
+	
+	def __set__(self, obj, val):
+		print 'Updating' , self.name
+		self.val = val
+
+class MyClass(object):
+	x = RevealAccess(10, 'var "x"')
+	y = 5
+
+m = MyClass()
+print "isinstance(m, MyClass): ",isinstance(m, MyClass)
+print m.x
+m.x = 20
+print m.x
+print m.y
+
+class C(object):
+	def getx(self): return self.__x
+	def setx(self, value): self.__x = value
+	def delx(self): del self.__x
+	x = property(getx, setx, delx, "I'm the 'x' property.")
+
+c = C()
+#print c.x
+c.x = 10
+print c.x
+c.setx(10)
+print c.x
