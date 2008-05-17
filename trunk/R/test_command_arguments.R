92f79a10331bd5979092bde729bc39edfae9387b
yh <yh@1f031e46-900f-0410-8561-9f4ed0c18923>
2008-05-17 09:33:01 +0000

    #2008-05-17 test how to pass arguments to an R standalone program. $* stands for all the arguments on the shell commandline after $0
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@216 1f031e46-900f-0410-8561-9f4ed0c18923

---
+++ trunk/R/test_command_arguments.R
@@ -0,0 +1,9 @@
+#2008-05-17 test how to pass arguments to an R standalone program. $* stands for all the arguments on the shell commandline after $0
+R --vanilla --args $0 $* <<EOF
+
+command_args = commandArgs()	#command_args starts with ["/usr/lib/R/bin/exec/R", "--vanilla", "--args"]
+print(command_args)
+
+
+#EOF below is optional
+EOF
