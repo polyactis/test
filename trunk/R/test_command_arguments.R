288b5032f834988062b337a0da09e3c5088a06e2
yh <yh@1f031e46-900f-0410-8561-9f4ed0c18923>
2008-05-18 04:35:44 +0000

    #!/usr/bin/env Rscript is better than "R --vanilla --args $0 $* <<EOF"
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@217 1f031e46-900f-0410-8561-9f4ed0c18923

--- trunk/R/test_command_arguments.R
+++ trunk/R/test_command_arguments.R
@@ -1,9 +1,10 @@
+#!/usr/bin/env Rscript
 #2008-05-17 test how to pass arguments to an R standalone program. $* stands for all the arguments on the shell commandline after $0
-R --vanilla --args $0 $* <<EOF
+#R --vanilla --args $0 $* <<EOF
 
 command_args = commandArgs()	#command_args starts with ["/usr/lib/R/bin/exec/R", "--vanilla", "--args"]
 print(command_args)
 
 
 #EOF below is optional
-EOF
+#EOF
