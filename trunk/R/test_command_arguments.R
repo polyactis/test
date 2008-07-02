eff7b6f00c803f17bce43d21f9d489fb3cf91e0a
yh <yh@1f031e46-900f-0410-8561-9f4ed0c18923>
2008-07-02 05:35:19 +0000

    correct the comments of commandArgs() after using Rscript
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@222 1f031e46-900f-0410-8561-9f4ed0c18923

--- trunk/R/test_command_arguments.R
+++ trunk/R/test_command_arguments.R
@@ -2,7 +2,8 @@
 #2008-05-17 test how to pass arguments to an R standalone program. $* stands for all the arguments on the shell commandline after $0
 #R --vanilla --args $0 $* <<EOF
 
-command_args = commandArgs()	#command_args starts with ["/usr/lib/R/bin/exec/R", "--vanilla", "--args"]
+command_args = commandArgs()	#command_args starts with ["/usr/lib/R/bin/exec/R", "--slave", "--no-restore", "--file=../test/R/test_command_arguments.R", "--args"]
+program_name = substr(command_args[4], 8, nchar(command_args[4]))	#command_args[4] = "--file=../test/R/test_command_arguments.R"
 print(command_args)
 
 
