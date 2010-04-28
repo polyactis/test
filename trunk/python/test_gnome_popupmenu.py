161c5830616510ebb203003c1a83a72a13a66dd4
yh <yh@1f031e46-900f-0410-8561-9f4ed0c18923>
2010-04-28 05:04:00 +0000

    ## 2010-4-27 testing the combo_box_entry
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@232 1f031e46-900f-0410-8561-9f4ed0c18923

--- trunk/python/test_gnome_popupmenu.py
+++ trunk/python/test_gnome_popupmenu.py
@@ -5,15 +5,14 @@
 	http://www.nabble.com/popup-menu-in-gtk.statusicon-under-windows-td19518682.html
 	
 	author description:
-		a script that create a gtk.statusicon, and create a menu that
-		is shown when I right click.
+		a script that create a gtk.statusicon, and create a menu that pops up upon right click.
 		
 		In this menu, there is a submenu, which contain a second menuitem.
 	
 		The callback of this second menuitem is not called under windows, but it
 		is under linux. Is it a bug in windows ? 
 """
-import gtk
+import gtk, gobject
 import pygtk
 
 def quit_cb(widget, data = None):
@@ -29,6 +28,14 @@ def popup_menu_cb(widget, button, time, data = None):
 		if data:
 			data.show_all()
 			data.popup(None, None, None, 3, time)
+
+def combo_box_changed(combobox, user_param1=None,):
+	"""
+	2010-4-27
+	"""
+	print combobox.get_active()
+	print combobox.get_active_text()
+	
 
 statusIcon = gtk.StatusIcon()
 
@@ -49,5 +56,26 @@ statusIcon.set_from_stock(gtk.STOCK_HOME)
 statusIcon.set_tooltip("StatusIcon test")
 statusIcon.connect('popup-menu', popup_menu_cb, menu)
 statusIcon.set_visible(True)
+
+## 2010-4-27 testing the combo_box_entry
+w = gtk.Window()
+w.connect("delete_event", gtk.main_quit)
+w.connect("destroy_event", gtk.main_quit)
+
+vbox = gtk.VBox()
+w.add(vbox)
+w.set_position(gtk.WIN_POS_CENTER)
+
+combo_box_entry =  gtk.ComboBoxEntry()
+combo_box_entry.connect("changed", combo_box_changed)
+vbox.pack_start(combo_box_entry)
+
+liststore = gtk.ListStore(str)	# str=gobject.TYPE_STRING
+combo_box_entry.set_model(liststore)
+combo_box_entry.set_text_column(0)	# must. default is -1, which would render later text-appending invisible.
+combo_box_entry.append_text("423")
+combo_box_entry.append_text("dumbass")
+
+w.show_all()
 
 gtk.main()
