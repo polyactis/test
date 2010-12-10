5c2fa5f14dd8b05a779a17ef5eb33985053c3cb4
yh <yh@1f031e46-900f-0410-8561-9f4ed0c18923>
2010-12-10 17:58:26 +0000

    improved version. final version
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@238 1f031e46-900f-0410-8561-9f4ed0c18923

---
+++ trunk/kk2010FallFortran/filld.for
@@ -0,0 +1,49 @@
+      SUBROUTINE FILLD(NX, NY, U, V, D, DX, DY, DTIM, DVE)
+C
+C     EVALUATES RIGHT-HAND SIDE OF THE
+C     UNSTEADY HEAT CONDUCTION EQUATION
+C
+	IMPLICIT NONE
+	REAL, DIMENSION (41,41) :: D, U, V
+	REAL, DIMENSION (1600), INTENT(OUT) :: DVE
+	REAL, INTENT(IN) :: DX, DY, DTIM
+	INTEGER, INTENT(IN) :: NX, NY
+	INTEGER :: K, J
+C     COMPUTE D(K,J)
+	DO 20 K=1,NY
+	DO 19 J=1,NX
+	D(K,J) = 0.
+   19	CONTINUE
+   20 CONTINUE
+     
+      DO 22 K=2,NY
+	DO 21 J=2,NX 
+	D(K,J) = (U(K,J) - U(K,J-1))/DX
+   	D(K,J) = D(K,J) + (V(K,J) - V(K-1,J))/DY
+   	D(K,J) = -1.*D(K,J)/DTIM
+   21	CONTINUE
+   22 CONTINUE
+
+	DO 23 J=1,NX
+		D(1,J) = 0.
+		D(NY,J) =0.
+   23	CONTINUE
+   
+   	DO 24 K=1,NY
+	D(K,1) = 0.
+   24	D(K,NX) =0.
+
+	D(NY-1,NX) = D(NY-1,NX) + 1./(DY*DY)
+	D(NY,NX-1) = D(NY,NX-1) + 1./(DX*DX)
+	D(NY,NX) = 1.
+
+C	CONVERT D(J,K) TO DVE(M)
+
+C	DO 10 J = 1,NX
+C	DO 9 K = 1,NY
+C	DVE((J-1)*NY+K) = D(K,J)
+C     9	CONTINUE
+C    10	CONTINUE
+	D = TRANSPOSE(D)
+      RETURN
+      END SUBROUTINE FILLD
--- trunk/kk2010FallFortran/press.for
+++ trunk/kk2010FallFortran/press.for
@@ -4,9 +4,11 @@ C
 C     EVALUATES THE COEEFICIANT MATRIX OF THE PRUSSURE EQUATIONS
 C	AND CONVERT MATRIX D(J,K) TO VECTOR DVE(M)
 C
+	REAL*8 aa,b,c,dd,e
 	DIMENSION D(41,41),DVE(1600),A(1600,1600)			  
 	COMMON DX,DY,NX,NY,R,U,DU,V,DV,P,FU,FV,FUP,FVP,ALF,CCXA,CCYA,DVE
 C
+
 	NXP=NX-1
 	NYP=NY-1
       NXY = NX*NY
@@ -15,17 +17,23 @@ C	DVE(M) = 0.
       DO 1 N = 1,NXY
     1 A(N,M) = 0.
     2 CONTINUE
+	
+	aa = -1./(DX*DX)
+	b = -1./(DY*DY)
+	c = 2./(DX*DX) + 2./(DY*DY)
+	dd = -1./(DY*DY)
+	e = -1./(DX*DX)
 C
 C     FOR INNER NODES
       DO 4 J = 2,NXP
 	JM = J - 1
 	DO 3 K = 2,NYP
 	M = JM*NY + K
-      A(M,M) =  2./(DX*DX) + 2./(DY*DY)
-	A(M-1,M) = -1./(DY*DY)
-	A(M+1,M) = -1./(DY*DY)
-      A(M-NY,M) = -1./(DX*DX)
-	A(M+NY,M) = -1./(DX*DX)
+	A(JM*NY+K, M) = c
+	A((JM-1)*NY+K, M) = aa
+	A(JM*NY+K-1, M) = b
+	A((JM+1)*NY+K, M) = dd
+	A(JM*NY+K+1, M) = e
     3 CONTINUE
     4 CONTINUE
 
@@ -40,53 +48,50 @@ C     FOR (1,NY) NODE
 	A(NY+NY,NY) = -1.
 
 C     FOR (NX,1) NODE
-	A((NX-1)*NY+1,(NX-1)*NY+1) = 2.
-	A((NX-1)*NY+2,(NX-1)*NY+1) = -1.
-	A((NX-2)*NY+1,(NX-1)*NY+1) = -1.
+	A((NX-1)*NY+1, (NX-1)*NY+1) = 2.
+	A((NX-2)*NY+1, (NX-1)*NY+1) = -1.
+	A((NX-1)*NY+2, (NX-1)*NY+1) = -1.
 
 C	FOR (NX,NY) NODE
       A(NXY,NXY) = 1.
 
 C	LEFT BOUNDARY NODES
 	DO 5 K= 2,NYP
-	A(K,K) = 2./(DX*DX) + 2./(DY*DY)
-	A(K-1,K) = -1./(DY*DY)
-	A(K+1,K) = -1./(DY*DY)
-	A(K+NY,K) = -2./(DX*DX)
+	M = K
+	A(K-1, M) = aa
+	A(K, M) = c
+	A(K+1, M) = dd
+	A(K+NY, M) = aa + e
     5	CONTINUE
 
 C	LOWER BOUNDARY NODES
 	DO 6 J=2,NXP
-	A((J-1)*NY+1,(J-1)*NY+1) = 2./(DX*DX) + 2./(DY*DY)
-	A((J-1)*NY+2,(J-1)*NY+1) = -2./(DY*DY)
-	A(J*NY+1,(J-1)*NY+1) = -1./(DX*DX)
-	A((J-2)*NY+2,(J-1)*NY+1) = -1./(DX*DX)
+	M = (J-1)*NY+1
+	A((J-2)*NY+1, M) = aa
+	A((J-1)*NY+1, M) = c
+	A((J-1)*NY+2, M) = b+dd
+	A(J*NY+1, M) = e
     6	CONTINUE
 
 C	RIGHT BOUNDARY NODES
 	DO 7 K= 2,NYP
- 	A((NX-1)*NY+K,(NX-1)*NY+K) = 2./(DX*DX) + 2./(DY*DY)
-	A((NX-1)*NY+K-1,(NX-1)*NY+K) = -1./(DY*DY)
-	A((NX-1)*NY+K+1,(NX-1)*NY+K) = -1./(DY*DY)
-	A((NX-2)*NY+K,(NX-1)*NY+K) = -2./(DX*DX)
+	M = (NX-1)*NY + K
+	A((NX-2)*NY+K, M) = aa+e
+	A((NX-1)*NY+K-1, M) = b
+ 	A((NX-1)*NY+K, M) = c
+	A((NX-1)*NY+K+1, M) = dd
     7	CONTINUE
 
 C	UPPER BOUNDARY NODES
 	DO 8 J= 2,NXP
- 	A(J*NY,J*NY) = 2./(DX*DX) + 2./(DY*DY)
-	A(J*NY-1,J*NY) = -2./(DY*DY)
-	A((J-1)*NY,J*NY) = -1./(DX*DX)
-	A((J+1)*NY,J*NY) = -1./(DX*DX)
+ 	M = (J-1)*NY + NY
+	A((J-2)*NY+NY, M) = aa
+	A((J-1)*NY+NY-1, M) = b+dd
+	A((J-1)*NY+NY, M) = c
+	A((J+1-1)*NY+NY, M) = e
     8	CONTINUE
 
-C	CONVERT D(J,K) TO DVE(M)
-
-C	DO 10 J = 1,NX
-C	DO 9 K = 1,NY
-C	DVE((J-1)*NY+K) = D(K,J)
-C    9	CONTINUE
-C   10	CONTINUE
-C
+	A = TRANSPOSE(A)
       RETURN
       END
 
