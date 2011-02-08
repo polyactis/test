1650b348422018cb8d35594546307cc82b765850
yh <yh@1f031e46-900f-0410-8561-9f4ed0c18923>
2011-02-08 23:54:26 +0000

    test more openmp function
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@248 1f031e46-900f-0410-8561-9f4ed0c18923

--- trunk/openmp/helloworld_cpp.cpp
+++ trunk/openmp/helloworld_cpp.cpp
@@ -3,6 +3,12 @@
 #include <sstream>
 int main(int argc, char *argv[]) {
 	int th_id, nthreads;
+	int maxNThreads;
+	#ifdef _OPENMP
+		std::cout << "Have OpenMP support\n";
+	#else
+		std::cout <<"no OpenMP support\n";
+	#endif
 #pragma omp parallel private(th_id)
 	{
 		th_id = omp_get_thread_num();
@@ -13,7 +19,8 @@ int main(int argc, char *argv[]) {
 #pragma omp master
 		{
 			nthreads = omp_get_num_threads();
-			std::cout << "There are " << nthreads << " threads" << std::endl;
+			maxNThreads = omp_get_max_threads();
+			std::cout << "There are " << maxNThreads << " max no of threads. " << nthreads << " threads. " << " version: " << _OPENMP << std::endl;
 		}
 	}
 	return 0;
