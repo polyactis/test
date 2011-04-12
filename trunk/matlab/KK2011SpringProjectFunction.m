3a2457f498db720eb22fbc71af1a8523ed035fbc
yh <yh@1f031e46-900f-0410-8561-9f4ed0c18923>
2011-04-12 01:34:40 +0000

    fix a bug. 	variable name "epsilon" spelled as episilon
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@255 1f031e46-900f-0410-8561-9f4ed0c18923

--- trunk/matlab/KK2011SpringProjectFunction.m
+++ trunk/matlab/KK2011SpringProjectFunction.m
@@ -4,14 +4,14 @@ function [value]=KK2011SpringProjectFunction(epsilon, max_n, max_m)
 % It can be used either in the multiple sample or the single sample
 % approach.
 
-value = calculateFirstTerm(episilon, max_n);
+value = calculateFirstTerm(epsilon, max_n);
 
-value = value + calculateFirstTerm(episilon, max_m);
+value = value + calculateFirstTerm(epsilon, max_m);
 
 for n=1:max_n
 	for m=1:max_m
-		x = 2*sqrt(pi)*n*episilon;
-		y = 2*sqrt(pi)*m*episilon;
+		x = 2*sqrt(pi)*n*epsilon;
+		y = 2*sqrt(pi)*m*epsilon;
 		z = sqrt(x*x + y*y);
 		value = value + 8*sin(z)*Besselj(1, z)/((m*m+n*n)*pi*pi*z);
 	end
@@ -20,11 +20,11 @@ disp(value);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [value] =calculateFirstTerm(episilon, max_n)
+function [value] =calculateFirstTerm(epsilon, max_n)
 
 value = 0;
 for n=1:max_n
-  x = 2*sqrt(pi)*n*episilon;
+  x = 2*sqrt(pi)*n*epsilon;
   value = value + 4*sin(x)*Besselj(1, x)/(n*n*pi*pi*x);
 end
 
