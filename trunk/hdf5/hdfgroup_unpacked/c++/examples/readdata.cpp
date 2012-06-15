d9ce76ce3ba50f5e0cd2d38de59d7ff312e8d833
yh <yh@1f031e46-900f-0410-8561-9f4ed0c18923>
2012-06-15 18:59:58 +0000

    numerous tests with HDF5
    git-svn-id: svn+ssh1999://crocea.mednet.ucla.edu/usr/local/svn/test@269 1f031e46-900f-0410-8561-9f4ed0c18923

--- trunk/hdf5/hdfgroup_unpacked/c++/examples/readdata.cpp
+++ trunk/hdf5/hdfgroup_unpacked/c++/examples/readdata.cpp
@@ -18,205 +18,211 @@
 //      two-dimensional plane of a three-dimensional array.  Various
 //      information about the dataset in the SDS.h5 file is obtained.
 //
-
 #ifdef OLD_HEADER_FILENAME
 #include <iostream.h>
 #else
 #include <iostream>
 #endif
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
 
 #ifndef H5_NO_NAMESPACE
 #ifndef H5_NO_STD
-    using std::cout;
-    using std::endl;
+using std::cout;
+using std::endl;
 #endif  // H5_NO_STD
 #endif
 
 #include "H5Cpp.h"
 
 #ifndef H5_NO_NAMESPACE
-    using namespace H5;
+using namespace H5;
 #endif
 
-const H5std_string FILE_NAME( "SDS.h5" );
-const H5std_string DATASET_NAME( "IntArray" );
-const int    NX_SUB = 3;	// hyperslab dimensions
-const int    NY_SUB = 4;
-const int    NX = 7;		// output buffer dimensions
-const int    NY = 7;
-const int    NZ = 3;
-const int    RANK_OUT = 3;
+const H5std_string FILE_NAME("SDS.h5");
+const H5std_string DATASET_NAME("IntArray");
+const int NX_SUB = 3; // hyperslab dimensions
+const int NY_SUB = 4;
+const int NX = 7; // output buffer dimensions
+const int NY = 4;	//2012.2.29 this number could be any number that is >=NY_SUB
+const int NZ = 3;
+const int RANK_OUT = 2;
 
-int main (void)
-{
-   /*
-    * Output buffer initialization.
-    */
-   int i, j, k;
-   int         data_out[NX][NY][NZ ]; /* output buffer */
-   for (j = 0; j < NX; j++)
-   {
-      for (i = 0; i < NY; i++)
-      {
- 	 for (k = 0; k < NZ ; k++)
-	    data_out[j][i][k] = 0;
-      }
-   }
+int main(void) {
+	/*
+	 * Output buffer initialization.
+	 */
+	int i, j, k;
+	//int data_out[NX][NY][NZ]; /* output buffer */
+	/*
+	int*** data_out;
+	data_out = (int***) malloc(NX * sizeof(int**));
+	for (j = 0; j < NX; j++) {
+		data_out[j] = (int**) malloc(NY * sizeof(int*));
+		for (i = 0; i < NY; i++) {
+			data_out[j][i] = (int*) malloc (NZ * sizeof(int));
+			for (k = 0; k < NZ; k++)
+				data_out[j][i][k] = 0;	//one-dimension				data_out[(j*NY+i)*NZ+k] = 0;
+		}
+	}
+	 */
+	int* data_out;
+	//data_out = (int*) malloc(NX*NY * sizeof(int));
+	data_out = new int[NX*NY];
+	/*
+	for (j = 0; j < NX; j++) {
+		for (i = 0; i < NY; i++) {
+			data_out[j*NY+i] = 0;
+		}
+	}*/
 
-   /*
-    * Try block to detect exceptions raised by any of the calls inside it
-    */
-   try
-   {
-      /*
-       * Turn off the auto-printing when failure occurs so that we can
-       * handle the errors appropriately
-       */
-      Exception::dontPrint();
 
-      /*
-       * Open the specified file and the specified dataset in the file.
-       */
-      H5File file( FILE_NAME, H5F_ACC_RDONLY );
-      DataSet dataset = file.openDataSet( DATASET_NAME );
+	/*
+	 * Try block to detect exceptions raised by any of the calls inside it
+	 */
+	try {
+		/*
+		 * Turn off the auto-printing when failure occurs so that we can
+		 * handle the errors appropriately
+		 */
+		Exception::dontPrint();
 
-      /*
-       * Get the class of the datatype that is used by the dataset.
-       */
-      H5T_class_t type_class = dataset.getTypeClass();
+		/*
+		 * Open the specified file and the specified dataset in the file.
+		 */
+		H5File file(FILE_NAME, H5F_ACC_RDONLY);
+		DataSet dataset = file.openDataSet(DATASET_NAME);
 
-      /*
-       * Get class of datatype and print message if it's an integer.
-       */
-      if( type_class == H5T_INTEGER )
-      {
-	 cout << "Data set has INTEGER type" << endl;
+		/*
+		 * Get the class of the datatype that is used by the dataset.
+		 */
+		H5T_class_t type_class = dataset.getTypeClass();
 
-         /*
-	  * Get the integer datatype
-          */
-	 IntType intype = dataset.getIntType();
+		/*
+		 * Get class of datatype and print message if it's an integer.
+		 */
+		if (type_class == H5T_INTEGER) {
+			cout << "Data set has INTEGER type" << endl;
 
-         /*
-          * Get order of datatype and print message if it's a little endian.
-          */
-	 H5std_string order_string;
-         H5T_order_t order = intype.getOrder( order_string );
-	 cout << order_string << endl;
+			/*
+			 * Get the integer datatype
+			 */
+			IntType intype = dataset.getIntType();
 
-         /*
-          * Get size of the data element stored in file and print it.
-          */
-         size_t size = intype.getSize();
-         cout << "Data size is " << size << endl;
-      }
+			/*
+			 * Get order of datatype and print message if it's a little endian.
+			 */H5std_string order_string;
+			H5T_order_t order = intype.getOrder(order_string);
+			cout << order_string << endl;
 
-      /*
-       * Get dataspace of the dataset.
-       */
-      DataSpace dataspace = dataset.getSpace();
+			/*
+			 * Get size of the data element stored in file and print it.
+			 */
+			size_t size = intype.getSize();
+			cout << "Data size is " << size << endl;
+		}
 
-      /*
-       * Get the number of dimensions in the dataspace.
-       */
-      int rank = dataspace.getSimpleExtentNdims();
+		/*
+		 * Get dataspace of the dataset.
+		 */
+		DataSpace dataspace = dataset.getSpace();
 
-      /*
-       * Get the dimension size of each dimension in the dataspace and
-       * display them.
-       */
-      hsize_t dims_out[2];
-      int ndims = dataspace.getSimpleExtentDims( dims_out, NULL);
-      cout << "rank " << rank << ", dimensions " <<
-	      (unsigned long)(dims_out[0]) << " x " <<
-	      (unsigned long)(dims_out[1]) << endl;
+		/*
+		 * Get the number of dimensions in the dataspace.
+		 */
+		int rank = dataspace.getSimpleExtentNdims();
 
-      /*
-       * Define hyperslab in the dataset; implicitly giving strike and
-       * block NULL.
-       */
-      hsize_t      offset[2];	// hyperslab offset in the file
-      hsize_t      count[2];	// size of the hyperslab in the file
-      offset[0] = 1;
-      offset[1] = 2;
-      count[0]  = NX_SUB;
-      count[1]  = NY_SUB;
-      dataspace.selectHyperslab( H5S_SELECT_SET, count, offset );
+		/*
+		 * Get the dimension size of each dimension in the dataspace and
+		 * display them.
+		 */
+		hsize_t dims_out[2];
+		int ndims = dataspace.getSimpleExtentDims(dims_out, NULL);
+		cout << "rank " << rank << ", dimensions "
+				<< (unsigned long) (dims_out[0]) << " x "
+				<< (unsigned long) (dims_out[1]) << endl;
 
-      /*
-       * Define the memory dataspace.
-       */
-      hsize_t     dimsm[3];              /* memory space dimensions */
-      dimsm[0] = NX;
-      dimsm[1] = NY;
-      dimsm[2] = NZ ;
-      DataSpace memspace( RANK_OUT, dimsm );
+		/*
+		 * Define hyperslab in the dataset; implicitly giving strike and
+		 * block NULL.
+		 */
+		hsize_t offset[2]; // hyperslab offset in the file
+		hsize_t count[2]; // size of the hyperslab in the file
+		offset[0] = 1;
+		offset[1] = 2;
+		count[0] = NX_SUB;
+		count[1] = NY_SUB;
+		dataspace.selectHyperslab(H5S_SELECT_SET, count, offset);
 
-      /*
-       * Define memory hyperslab.
-       */
-      hsize_t      offset_out[3];	// hyperslab offset in memory
-      hsize_t      count_out[3];	// size of the hyperslab in memory
-      offset_out[0] = 3;
-      offset_out[1] = 0;
-      offset_out[2] = 0;
-      count_out[0]  = NX_SUB;
-      count_out[1]  = NY_SUB;
-      count_out[2]  = 1;
-      memspace.selectHyperslab( H5S_SELECT_SET, count_out, offset_out );
+		/*
+		 * Define the memory dataspace.
+		 */
+		hsize_t dimsm[2]; /* memory space dimensions */
+		dimsm[0] = NX;
+		dimsm[1] = NY;
+		//dimsm[2] = NZ;
+		DataSpace memspace(RANK_OUT, dimsm);
 
-      /*
-       * Read data from hyperslab in the file into the hyperslab in
-       * memory and display the data.
-       */
-      dataset.read( data_out, PredType::NATIVE_INT, memspace, dataspace );
+		/*
+		 * Define memory hyperslab.
+		 */
+		hsize_t offset_out[2]; // hyperslab offset in memory
+		hsize_t count_out[2]; // size of the hyperslab in memory
+		offset_out[0] = 0;	// 2012.2.29 yh: could be anything as far as this number + NX_SUB is still within this memspace dimension
+		offset_out[1] = 0;
+		//offset_out[2] = 0;
+		count_out[0] = NX_SUB;
+		count_out[1] = NY_SUB;
+		//count_out[2] = 1;
+		memspace.selectHyperslab(H5S_SELECT_SET, count_out, offset_out);
 
-      for (j = 0; j < NX; j++)
-      {
-	for (i = 0; i < NY; i++)
-	   cout << data_out[j][i][0] << " ";
-	cout << endl;
-      }
-      /*
-       * 0 0 0 0 0 0 0
-       * 0 0 0 0 0 0 0
-       * 0 0 0 0 0 0 0
-       * 3 4 5 6 0 0 0
-       * 4 5 6 7 0 0 0
-       * 5 6 7 8 0 0 0
-       * 0 0 0 0 0 0 0
-       */
-   }  // end of try block
+		/*
+		 * Read data from hyperslab in the file into the hyperslab in
+		 * memory and display the data.
+		 */
+		dataset.read(data_out, PredType::NATIVE_INT, memspace, dataspace);
 
-   // catch failure caused by the H5File operations
-   catch( FileIException error )
-   {
-      error.printError();
-      return -1;
-   }
+		for (j = 0; j < NX; j++) {
+			for (i = 0; i < NY; i++)
+				cout << data_out[j*NY+i] << " ";
+			cout << endl;
+		}
+		/*
+		 * 0 0 0 0 0 0 0
+		 * 0 0 0 0 0 0 0
+		 * 0 0 0 0 0 0 0
+		 * 3 4 5 6 0 0 0
+		 * 4 5 6 7 0 0 0
+		 * 5 6 7 8 0 0 0
+		 * 0 0 0 0 0 0 0
+		 */
+	} // end of try block
 
-   // catch failure caused by the DataSet operations
-   catch( DataSetIException error )
-   {
-      error.printError();
-      return -1;
-   }
+	// catch failure caused by the H5File operations
+	catch (FileIException error) {
+		error.printError();
+		return -1;
+	}
 
-   // catch failure caused by the DataSpace operations
-   catch( DataSpaceIException error )
-   {
-      error.printError();
-      return -1;
-   }
+	// catch failure caused by the DataSet operations
+	catch (DataSetIException error) {
+		error.printError();
+		return -1;
+	}
 
-   // catch failure caused by the DataSpace operations
-   catch( DataTypeIException error )
-   {
-      error.printError();
-      return -1;
-   }
+	// catch failure caused by the DataSpace operations
+	catch (DataSpaceIException error) {
+		error.printError();
+		return -1;
+	}
 
-   return 0;  // successfully terminated
+	// catch failure caused by the DataSpace operations
+	catch (DataTypeIException error) {
+		error.printError();
+		return -1;
+	}
+	delete [] data_out;
+	return 0; // successfully terminated
 }
 
